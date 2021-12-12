42c1eed3e8f806923081e2e1048def9865cd66b1
kalashshah <202051096@iiitvadodara.ac.in>
2021-12-12 16:46:06 +0530

    Add music button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,60 +17,96 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Home',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
 
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Home',
-            style: TextStyle(
-            fontSize: 40,
-              fontWeight: FontWeight.bold
-        ),),
-            Carousel(),
+              Carousel(),
 
-            Padding(
-              padding: const EdgeInsets.only(top: 20,bottom: 10),
-              child: Text('Memories',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold
+              Text(
+                'Music',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
 
-              ),),
-            ),
-
-            SampleCards(),
-
-            MemoriesList(),
-
-
-
-            Text('Music',style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-
-
-            ),),
-
-            ElevatedButton(onPressed:(){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>MusicPage()));
-            } ,
-                child: Text('Go to Music page'))
-          ],
-
-
-
+              TextButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 10,
+                ),
+                onPressed: () { 
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MusicPage()),
+                  );
+                 },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.black,
+                  elevation: 10,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: ListTile(
+                          leading: Icon(Icons.album, size: 60, color: Colors.white),
+                          title:
+                              Padding(
+                                padding: EdgeInsets.only(top: 10, bottom: 10),
+                                child: Text('Be Happier', style: TextStyle(fontSize: 20.0, color: Colors.white)),
+                              ),
+                          subtitle: Text('Music is the key to the soul',
+                              style: TextStyle(fontSize: 18.0, color: Colors.white)),
+                        ),
+                      ),
+                      // ButtonBar(
+                      //   children: <Widget>[
+                      //     RaisedButton(
+                      //       child: const Text('Play'),
+                      //       onPressed: () {/* ... */},
+                      //     ),
+                      //     RaisedButton(
+                      //       child: const Text('Pause'),
+                      //       onPressed: () {/* ... */},
+                      //     ),
+                      //   ],
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+              // ElevatedButton(
+              //     onPressed: () {
+              //       Navigator.push(context,
+              //           MaterialPageRoute(builder: (context) => MusicPage()));
+              //     },
+              //     child: Text('Go to Music page')),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Text(
+                  'Memories',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SampleCards(),
+              MemoriesList(),
+            ],
+          ),
         ),
       ),
-
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){
-
+        onPressed: () {
           Navigator.of(context).pushNamed(AddMemories.routeName);
-
         },
-
       ),
     );
   }
